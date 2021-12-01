b498e80428c6adde71dbb85a16c8c0d9599adc8d
domagoj.vukovic <off.vukovic@gmail.com>
2021-12-01 14:31:16 +0100

    feat: improve docs

--- src/rules/import-declaration-newline.ts
+++ src/rules/import-declaration-newline.ts
@@ -8,7 +8,7 @@ const value = createRule({
     create(context) {
         return {
             ImportDeclaration(node: TSESTree.ImportDeclaration) {
-                // If only 1 import skip
+                // If only 1 import exit
                 if (node.specifiers.length < 2) {
                     return
                 }
@@ -16,13 +16,16 @@ const value = createRule({
                 const sourceCode = context.getSourceCode()
 
                 // Iterate trough each import node
-                // E.g. import { A, B, C } from 'D' should go iterate A, B, C
+                // E.g. import { A, B, C } from 'D' should iterate A, B, C
                 for (let index = 1; index < node.specifiers.length; index++) {
+                    // Take the current node (starting at 1) and previous node (0)
+                    // E.g. import React, { useEffect, useState } from 'react'
+                    //             ^^^^^    ^^^^^^^^^
                     const previousNode = node.specifiers[index - 1]
                     const currentNode = node.specifiers[index]
 
-                    // E.g import { A, B, C } from 'D'
-                    // Its looking at A and B
+                    // E.g. import React, { useEffect, useState } from 'react'
+                    // It's looking at React and useEffect
                     const areMultipleImportsOnSameLine = currentNode.loc.start.line === previousNode.loc.start.line
 
                     if (areMultipleImportsOnSameLine) {
@@ -32,7 +35,7 @@ const value = createRule({
                             currentNode.type === TSESTree.AST_NODE_TYPES.ImportSpecifier &&
                             previousNode.type === TSESTree.AST_NODE_TYPES.ImportDefaultSpecifier
                         ) {
-                            // Following is the case hence nothing to fix
+                            // If there is default and named import, exit
                             // import React, { useState } from 'react'
                             if (node.specifiers.length <= 2) {
                                 return null
@@ -64,36 +67,33 @@ const value = createRule({
                         } else {
                             // Token from import node
                             // E.g. import { A, B, C } from 'D' => this will be A
-                            const firstToken = sourceCode.getFirstToken(currentNode)
+                            const token = sourceCode.getFirstToken(currentNode)
 
                             // Exit if no named imports in braces or a single import
                             // E.g. import {} from 'react'
-                            // E.g. import { useState } from 'react'
-                            if (!firstToken) {
+                            if (!token) {
                                 return
                             }
 
                             // Get comma before the named import
                             // E.g. import { useState, useEffect } from 'react'
                             //                       ^
-                            const comma = sourceCode.getTokenBefore(firstToken)
+                            const comma = sourceCode.getTokenBefore(token)
 
                             // First named import shouldn't have a comma before it so exit
                             // E.g. import { useState, useEffect } from 'react'
-                            //               ^^^^^^^^ => no comma before that word
+                            //               ^^^^^^^^ => no comma before that variable
                             if (!comma) {
                                 return
                             }
 
+                            // Range from comma until the next variable
+                            // E.g. import { useState, useEffect } from 'react'
+                            //                        ^
                             const rangeAfterComma: TSESTree.Range = [
                                 comma.range[1],
-                                firstToken.range[0],
+                                token.range[0],
                             ]
-
-                            // Don't fix comments between the comma and the next import
-                            if (sourceCode.text.slice(rangeAfterComma[0], rangeAfterComma[1]).trim()) {
-                                return null
-                            }
 
                             // Replace comma with comma with new line
                             // E.g. useEffect, will be replaced with useEffect,\n
