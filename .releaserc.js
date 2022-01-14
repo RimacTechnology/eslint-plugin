fbaaed1b88a50486baa59b5cdedf8a420783d850
mario.jerkovic <mario.jerkovic@rimac-automobili.com>
2022-01-14 09:46:13 +0100

    chore(.releaserc): type fix for release rules on commit analyzer

--- .releaserc.js
+++ .releaserc.js
@@ -25,7 +25,7 @@ module.exports = {
                         release: 'patch',
                     },
                     {
-                        tag: 'chore',
+                        type: 'chore',
                         release: 'minor',
                     },
                     {
