97728dd7af3f779ca12e54f199cab8f9b84d4b30
domagoj.vukovic <off.vukovic@gmail.com>
2022-07-12 15:50:02 +0200

    feat: wip

--- src/rules/class-members-sorting.ts
+++ src/rules/class-members-sorting.ts
@@ -1,11 +1,48 @@
+import { ClassElement } from '@typescript-eslint/types/dist/generated/ast-spec'
+import { AST_NODE_TYPES } from '@typescript-eslint/utils'
 import { createRule } from '../utils'
 
 const NAME = 'class-members-sorting'
 
+const MEMBER_ORDER = [
+    "static-properties",
+    "static-methods",
+    "public-properties",
+    "private-properties",
+    "constructor",
+    "getters",
+    "setters",
+    "public-methods",
+    "private-methods",
+    "other",
+] as const
+
+const getType = (element: ClassElement) => {
+    if (element.type === AST_NODE_TYPES.PropertyDefinition && element.static)
+    console.log(element.type.toString())
+}
+
 const value = createRule({
     create(context) {
         return {
-            ClassDeclaration() {
+            ClassBody(node) {
+                const nodes: Record<typeof MEMBER_ORDER[number], ClassElement[]> = {
+                    "private-methods": [],
+                    "private-properties": [],
+                    "public-methods": [],
+                    "public-properties": [],
+                    "static-methods": [],
+                    constructor: [],
+                    "static-properties": [],
+                    setters: [],
+                    getters: [],
+                    other: []
+                }
+
+                node.body.forEach((element) => {
+                    // TODO: handle type
+                    getType(element as ClassElement)
+                })
 
             },
         }
