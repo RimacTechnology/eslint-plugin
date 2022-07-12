7506b5ba5e97e1f6ebb6d5e5a02f00442e86df32
domagoj.vukovic <off.vukovic@gmail.com>
2022-07-12 11:54:12 +0200

    fix: add rule to global export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import documentTodos from './rules/document-todos'
 import importDeclarationNewline from './rules/import-declaration-newline'
 
 const configuration = {
     rules: {
         [importDeclarationNewline.name]: importDeclarationNewline.value,
+        [documentTodos.name]: documentTodos.value,
     },
 }
 
